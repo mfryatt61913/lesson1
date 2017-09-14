6cff02e4466c82d49b72f3140f459d79679a722b
61913 <mfryatt.61913@farnborough.ac.uk>
2017-09-14 10:21:03 +0100

    I changed it again I think

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world again!");
+        System.out.println("Hello world again! This might be a changed version, I don't know");
     }
 }
